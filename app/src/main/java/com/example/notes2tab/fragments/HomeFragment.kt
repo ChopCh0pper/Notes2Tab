4774f474a9fed9635c9e9d334051bc4da9ea9318
sks9 <sks9@tpu.ru>
2024-05-25 17:52:49 +0700

    problem with binding

--- app/src/main/java/com/example/notes2tab/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notes2tab/fragments/HomeFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.notes2tab.R
+import com.example.notes2tab.adapters.SongAdapter
+import com.example.notes2tab.dataModels.Song
+import com.example.notes2tab.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val songsList = mutableListOf<Song>()
+        songsList.add(Song("Звезда по имени солнце", "В. Цой"))
+        songsList.add(Song("Название песни 2", "Автор 2"))
+        songsList.add(Song("Название песни 3", "Автор 3"))
+
+        val adapter = SongAdapter(songsList)
+        binding.rvSongs.adapter = adapter
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
